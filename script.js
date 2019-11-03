9d6659fa1116b0b2b83158dad78d9c452c94389d
Matthew Bourgoine <mbourgoine21@gmail.com>
2019-11-02 21:56:27 -0700

    reset on html

--- script.js
+++ script.js
@@ -5,15 +5,71 @@
 // .then(contents => console.log(contents))
 // .catch(() => console.log("Can’t access " + url + " response. Blocked by browser?"))
 
-var name = prompt("What is your summoner name?", "Harry Potter!");
+//var name = prompt("What is your summoner name?", "Majestic Moose");
+var api = 'https://cors-anywhere.herokuapp.com/https://na1.api.riotgames.com/lol/';
+var api_summ = 'summoner/v4/summoners/by-name/';
+var api_key = '?api_key=RGAPI-9d50dff2-5141-4f69-9633-b16cd6a9d89d';
+// var accountID;
 
-$.getJSON("https://cors-anywhere.herokuapp.com/https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + name + "?api_key=RGAPI-9d50dff2-5141-4f69-9633-b16cd6a9d89d", function(data){
-    console.log(data);
+// $.getJSON(url + "summoner/v4/summoners/by-name/" + name + "?api_key=RGAPI-9d50dff2-5141-4f69-9633-b16cd6a9d89d", function(data){
 
-    var summoner_name = data.name;
-    var summoner_level = data.summonerLevel;
+//     console.log(data);
 
-    $(".summoner_name").append(summoner_name);
-    $(".summoner_level").append(summoner_level);
+//     var summoner_name = data.name;
+//     var summoner_level = data.summonerLevel;
+//     accountID = data.accountId;
+
+//     $(".summoner_name").append(summoner_name);
+//     $(".summoner_level").append(summoner_level);
+
+//     console.log(accountID);
+
+
+// });
+
+
+function setup(){
+
+    var button = select('#select');
+    button.mousePressed(summnameAsk);
+    var input = select('#summoner_name');
+}
+
+
+
+function summnameAsk(){
+    var url = api + api_summ + input.value + api_key;
+
+    loadJSON(url, gotData, 'jsonp');
+}
+
+function gotData(data){
+    summoner = data;
+}
+
+function output(){
+
+    if(summoner){
+        var summoner_name = summoner.name;
+        var summoner_level = summoner.summonerLevel;
+        var accountID = summoner.accountId;
+
+        $(".summoner_name").append(summoner_name);
+        $(".summoner_level").append(summoner_level);
+
+        console.log(accountID);
     }
-);
+}
+
+// if(accountID){
+//     $.getJSON(url + "match/v4/matchlists/by-account/" + accountID + "?api_key=RGAPI-9d50dff2-5141-4f69-9633-b16cd6a9d89d", function(matchData){
+//         console.log(matchData);
+
+//         var gamesID = matchData.matches[0].gameId;
+
+//         $(".entries").append(gamesID);
+//     });
+// }
+    
+    
+
